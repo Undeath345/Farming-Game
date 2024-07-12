1737e84b76efb5451c95aa3c3f5702cf82179e20
Nhat Thien <119486514+Kahili1@users.noreply.github.com>
2024-07-12 18:32:14 +0700

    no message

---
+++ Farming Sim/Assets/thien/Scripts/PlayerInteractable.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractable : MonoBehaviour
+{
+    PlayerController player;
+    Rigidbody2D rgbd2d;
+    [SerializeField] float offsetDistance = 1f;
+    [SerializeField] float sizeOfInteractableArea = 1.2f;
+    Character character;
+    private void Awake()
+    {
+        player = GetComponent<PlayerController>();
+        rgbd2d = GetComponent<Rigidbody2D>();
+        character = GetComponent<Character>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Interact();
+        }
+    }
+    private void Interact()
+    {
+        Vector2 postision = rgbd2d.position + player.lastMotionVector * offsetDistance;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(postision, sizeOfInteractableArea);
+        foreach (Collider2D collider in colliders)
+        {
+            Interactable hit = collider.GetComponent<Interactable>();
+            if (hit != null)
+            {
+                hit.Interact(character);
+                break;
+            }
+        }
+    }
+}
