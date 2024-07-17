c4e2d0a17906ab68714d0c3c725137e896d9262e
DESKTOP-7QHBA3R\Admin <noone8047@gmail.com>
2024-07-17 18:49:18 +0700

    no message

--- Farming Sim/Assets/thien/Scripts/CropsContainer.cs
+++ Farming Sim/Assets/thien/Scripts/CropsContainer.cs
@@ -2,13 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Data/Crops Container")]
 public class CropsContainer : MonoBehaviour
 {
 
-<<<<<<< HEAD
-    
-=======
-
->>>>>>> 4ebcfff8970ca9038b6c8345352bd429159a1a15
 }
