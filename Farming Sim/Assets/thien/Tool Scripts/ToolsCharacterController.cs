372de8322e9248201a1205725fd355661142719f
Nhat Thien <119486514+Kahili1@users.noreply.github.com>
2024-07-19 20:04:43 +0700

    no message

--- Farming Sim/Assets/thien/Tool Scripts/ToolsCharacterController.cs
+++ Farming Sim/Assets/thien/Tool Scripts/ToolsCharacterController.cs
@@ -7,7 +7,7 @@ using UnityEngine.Timeline;
 
 public class ToolsCharacterController : MonoBehaviour
 {
-    PlayerController character;
+    /*PlayerController character;
     Rigidbody2D rgbd2d;
     [SerializeField] float offsetDistance = 1.0f;
     [SerializeField] float sizeOfInteractableArea = 1.2f;
@@ -79,5 +79,5 @@ public class ToolsCharacterController : MonoBehaviour
         {
             cropsManager.Plow(Vector2Int)selectedTile);
         }
-    }
+    }*/
 }
