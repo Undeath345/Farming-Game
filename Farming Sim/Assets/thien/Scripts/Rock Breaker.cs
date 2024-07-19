fe8f4e2bf400fd33391e8401df95ba9ca7346241
Nhat Thien <119486514+Kahili1@users.noreply.github.com>
2024-07-19 19:00:25 +0700

    no message

---
+++ Farming Sim/Assets/thien/Scripts/Rock Breaker.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockBreaker : ToolHit
+{
+    [SerializeField] GameObject pickUpDrop;
+    public override void 
+
+}
