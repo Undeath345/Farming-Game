4d2280d6f33e7d7634202f7dd559017773c7352b
Nhat Thien <119486514+Kahili1@users.noreply.github.com>
2024-07-19 19:30:19 +0700

    no message

--- Farming Sim/Assets/Kiet/Script/RockBreaker.cs
+++ Farming Sim/Assets/Kiet/Script/RockBreaker.cs
@@ -2,10 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TreeCutter : ToolHit
+public class Rockbreaker: ToolHit
 {
     [SerializeField] GameObject pickUpDrop;
-    [SerializeField] GameObject pickDownDrop;
     [SerializeField] int dropCount = 5;
     [SerializeField] float spread = 0.7f;
     public override void Hit()
@@ -17,9 +16,8 @@ public class TreeCutter : ToolHit
             position.x += spread * UnityEngine.Random.value - spread / 2;
             position.y += spread * UnityEngine.Random.value - spread / 2;
             GameObject go = Instantiate(pickUpDrop);
-            GameObject go1 = Instantiate(pickDownDrop);
             go.transform.position = position;
-            go1.transform.position = position;
+
         }
         Destroy(gameObject);
     }
